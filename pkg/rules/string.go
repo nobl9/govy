75df8ff3c9465662daeda5876561f1e618f2e681
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2024-10-29 17:22:48 +0100

    shorten details

--- pkg/rules/string.go
+++ pkg/rules/string.go
@@ -486,7 +486,7 @@ func StringRegexp() govy.Rule[string] {
 	}).
 		WithErrorCode(ErrorCodeStringRegexp).
 		// nolint: lll
-		WithDetails(`the accepted regular expression syntax must comply to RE2, it is described at https://golang.org/s/re2syntax, except for \C; for an overview of the syntax, see https://pkg.go.dev/regexp/syntax`).
+		WithDetails(`the regular expression syntax must comply to RE2, it is described at https://golang.org/s/re2syntax, except for \C; for an overview of the syntax, see https://pkg.go.dev/regexp/syntax`).
 		WithDescription(msg)
 }
 
