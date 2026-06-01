edec7b8f43ed73cb52401667941a047814f6e122
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2026-06-01 22:13:05 +0200

    feat: add configurable URL validation options
    Support scheme, host, user info, and hostname allow/deny checks in the URL rule.
    Add targeted error messages and tests to cover the new validation behavior.

--- pkg/rules/url.go
+++ pkg/rules/url.go
@@ -3,15 +3,80 @@ package rules
 import (
 	"errors"
 	"net/url"
+	"slices"
 
 	"github.com/nobl9/govy/internal/messagetemplates"
 	"github.com/nobl9/govy/pkg/govy"
 )
 
+// URLOption configures additional checks for [URL].
+type URLOption interface {
+	apply(*urlRuleOptions)
+}
+
+type urlOptionFunc func(*urlRuleOptions)
+
+func (f urlOptionFunc) apply(options *urlRuleOptions) {
+	f(options)
+}
+
+type urlRuleOptions struct {
+	schemes       []string
+	hostRequired  bool
+	forbidUser    bool
+	hostAllowList []string
+	hostDenyList  []string
+}
+
+type urlTemplateVars struct {
+	Scheme            bool
+	HostRequired      bool
+	UserInfoForbidden bool
+	HostDenyList      bool
+	HostAllowList     bool
+}
+
+// URLSchemes restricts [URL] to the provided schemes.
+func URLSchemes(schemes ...string) URLOption {
+	return urlOptionFunc(func(options *urlRuleOptions) {
+		options.schemes = slices.Clone(schemes)
+	})
+}
+
+// URLHostRequired requires [URL] values to have a host.
+func URLHostRequired() URLOption {
+	return urlOptionFunc(func(options *urlRuleOptions) {
+		options.hostRequired = true
+	})
+}
+
+// URLUserInfoForbidden rejects [URL] values with user information.
+func URLUserInfoForbidden() URLOption {
+	return urlOptionFunc(func(options *urlRuleOptions) {
+		options.forbidUser = true
+	})
+}
+
+// URLHostAllowList restricts [URL] values to the provided hostnames.
+func URLHostAllowList(hosts ...string) URLOption {
+	return urlOptionFunc(func(options *urlRuleOptions) {
+		options.hostAllowList = slices.Clone(hosts)
+	})
+}
+
+// URLHostDenyList rejects [URL] values with the provided hostnames.
+// Denied entries take precedence over entries defined with [URLHostAllowList].
+func URLHostDenyList(hosts ...string) URLOption {
+	return urlOptionFunc(func(options *urlRuleOptions) {
+		options.hostDenyList = slices.Clone(hosts)
+	})
+}
+
 // URL ensures the URL is valid.
 // The URL must have a scheme (e.g. https://) and contain either host, fragment or opaque data.
-func URL() govy.Rule[*url.URL] {
+func URL(options ...URLOption) govy.Rule[*url.URL] {
 	tpl := messagetemplates.Get(messagetemplates.URLTemplate)
+	ruleOptions := newURLRuleOptions(options...)
 
 	return govy.NewRule(func(v *url.URL) error {
 		if err := validateURL(v); err != nil {
@@ -19,6 +84,9 @@ func URL() govy.Rule[*url.URL] {
 				PropertyValue: v,
 				Error:         err.Error(),
 			})
+		}
+		if vars, ok := validateURLOptions(v, ruleOptions); !ok {
+			return govy.NewRuleErrorTemplate(vars)
 		}
 		return nil
 	}).
@@ -37,4 +105,62 @@ func validateURL(u *url.URL) error {
 		return errors.New("valid URL must contain either host, fragment or opaque data")
 	}
 	return nil
+}
+
+func newURLRuleOptions(options ...URLOption) urlRuleOptions {
+	var ruleOptions urlRuleOptions
+	for _, option := range options {
+		if option != nil {
+			option.apply(&ruleOptions)
+		}
+	}
+	return ruleOptions
+}
+
+func validateURLOptions(u *url.URL, options urlRuleOptions) (govy.TemplateVars, bool) {
+	if len(options.schemes) > 0 && !containsString(options.schemes, u.Scheme) {
+		return govy.TemplateVars{
+			PropertyValue:   u,
+			ComparisonValue: options.schemes,
+			Custom:          urlTemplateVars{Scheme: true},
+		}, false
+	}
+
+	hostname := u.Hostname()
+	if options.hostRequired && hostname == "" {
+		return govy.TemplateVars{
+			PropertyValue: u,
+			Custom:        urlTemplateVars{HostRequired: true},
+		}, false
+	}
+	if options.forbidUser && u.User != nil {
+		return govy.TemplateVars{
+			PropertyValue: u,
+			Custom:        urlTemplateVars{UserInfoForbidden: true},
+		}, false
+	}
+	if len(options.hostDenyList) > 0 && containsString(options.hostDenyList, hostname) {
+		return govy.TemplateVars{
+			PropertyValue:   u,
+			ComparisonValue: options.hostDenyList,
+			Custom:          urlTemplateVars{HostDenyList: true},
+		}, false
+	}
+	if len(options.hostAllowList) > 0 && !containsString(options.hostAllowList, hostname) {
+		return govy.TemplateVars{
+			PropertyValue:   u,
+			ComparisonValue: options.hostAllowList,
+			Custom:          urlTemplateVars{HostAllowList: true},
+		}, false
+	}
+	return govy.TemplateVars{}, true
+}
+
+func containsString(values []string, value string) bool {
+	for _, v := range values {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
