6e0a5659e606c53a4cfd72850fbbba1e555c30a9
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2026-07-10 23:27:38 +0200

    refactor: inline string format rule constructors

--- pkg/rules/string.go
+++ pkg/rules/string.go
@@ -11,7 +11,6 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
-	"text/template"
 	"time"
 	"unicode"
 
@@ -317,52 +316,66 @@ func StringJSON() govy.Rule[string] {
 func StringMD5() govy.Rule[string] {
 	tpl := messagetemplates.Get(messagetemplates.StringMD5Template)
 
-	return newStringHashDigestRule(func(s string) bool {
-		return md5Regexp().MatchString(s)
-	}, ErrorCodeStringMD5, tpl)
-}
-
-// StringSHA256 ensures the property's value is a lowercase hexadecimal SHA-256 digest.
-func StringSHA256() govy.Rule[string] {
-	tpl := messagetemplates.Get(messagetemplates.StringSHA256Template)
-
-	return newStringHashDigestRule(func(s string) bool {
-		return sha256Regexp().MatchString(s)
-	}, ErrorCodeStringSHA256, tpl)
-}
-
-// StringSHA384 ensures the property's value is a lowercase hexadecimal SHA-384 digest.
-func StringSHA384() govy.Rule[string] {
-	tpl := messagetemplates.Get(messagetemplates.StringSHA384Template)
-
-	return newStringHashDigestRule(func(s string) bool {
-		return sha384Regexp().MatchString(s)
-	}, ErrorCodeStringSHA384, tpl)
-}
-
-// StringSHA512 ensures the property's value is a lowercase hexadecimal SHA-512 digest.
-func StringSHA512() govy.Rule[string] {
-	tpl := messagetemplates.Get(messagetemplates.StringSHA512Template)
-
-	return newStringHashDigestRule(func(s string) bool {
-		return sha512Regexp().MatchString(s)
-	}, ErrorCodeStringSHA512, tpl)
-}
-
-func newStringHashDigestRule(
-	validate func(string) bool,
-	errorCode govy.ErrorCode,
-	tpl *template.Template,
-) govy.Rule[string] {
 	return govy.NewRule(func(s string) error {
-		if !validate(s) {
+		if !md5Regexp().MatchString(s) {
 			return govy.NewRuleErrorTemplate(govy.TemplateVars{
 				PropertyValue: s,
 			})
 		}
 		return nil
 	}).
-		WithErrorCode(errorCode).
+		WithErrorCode(ErrorCodeStringMD5).
+		WithMessageTemplate(tpl).
+		WithDescription(mustExecuteTemplate(tpl, govy.TemplateVars{}))
+}
+
+// StringSHA256 ensures the property's value is a lowercase hexadecimal SHA-256 digest.
+func StringSHA256() govy.Rule[string] {
+	tpl := messagetemplates.Get(messagetemplates.StringSHA256Template)
+
+	return govy.NewRule(func(s string) error {
+		if !sha256Regexp().MatchString(s) {
+			return govy.NewRuleErrorTemplate(govy.TemplateVars{
+				PropertyValue: s,
+			})
+		}
+		return nil
+	}).
+		WithErrorCode(ErrorCodeStringSHA256).
+		WithMessageTemplate(tpl).
+		WithDescription(mustExecuteTemplate(tpl, govy.TemplateVars{}))
+}
+
+// StringSHA384 ensures the property's value is a lowercase hexadecimal SHA-384 digest.
+func StringSHA384() govy.Rule[string] {
+	tpl := messagetemplates.Get(messagetemplates.StringSHA384Template)
+
+	return govy.NewRule(func(s string) error {
+		if !sha384Regexp().MatchString(s) {
+			return govy.NewRuleErrorTemplate(govy.TemplateVars{
+				PropertyValue: s,
+			})
+		}
+		return nil
+	}).
+		WithErrorCode(ErrorCodeStringSHA384).
+		WithMessageTemplate(tpl).
+		WithDescription(mustExecuteTemplate(tpl, govy.TemplateVars{}))
+}
+
+// StringSHA512 ensures the property's value is a lowercase hexadecimal SHA-512 digest.
+func StringSHA512() govy.Rule[string] {
+	tpl := messagetemplates.Get(messagetemplates.StringSHA512Template)
+
+	return govy.NewRule(func(s string) error {
+		if !sha512Regexp().MatchString(s) {
+			return govy.NewRuleErrorTemplate(govy.TemplateVars{
+				PropertyValue: s,
+			})
+		}
+		return nil
+	}).
+		WithErrorCode(ErrorCodeStringSHA512).
 		WithMessageTemplate(tpl).
 		WithDescription(mustExecuteTemplate(tpl, govy.TemplateVars{}))
 }
